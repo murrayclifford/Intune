f35d46b45ec2cf278961a8b627c7dd66c53a0019
MurrayC <86817415+murrayclifford@users.noreply.github.com>
2022-05-19 13:41:02 +1000

    Updated detection script and renamed PS1 files

---
+++ Proactive Remediations/BitLocker/Detect-BitLockerRecoveryKeys.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+    Checks Event Logs for issues with escrow of BitLocker recovery keys to Azure AD
+
+#>
+
+Start-Transcript -Path "$Env:ProgramData\Microsoft\IntuneManagementExtension\Logs\Detect-BitLockerRecoveryKeys.log"
+
+Try {
+    $Result = Get-WinEvent -FilterHashTable @{LogName = "Microsoft-Windows-BitLocker/BitLocker Management"; StartTime = (Get-Date).Addseconds(-86400) } | Where-Object { ($_.id -eq 846) } | Format-Table message
+    $ID = $Result | Measure-Object
+    If ($ID.Count -lt 5) {
+        Write-Output "Bitlocker backup to azure add succeeded"
+        Exit 0
+    }
+    Else {
+        Write-Output $result
+        Exit 1
+    }
+}
+    
+catch
+{
+    Write-Warning "Value Missing"
+    Exit 1
+}
+
+Stop-Transcript
