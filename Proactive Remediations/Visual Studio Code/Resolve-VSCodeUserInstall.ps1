bf5d34f0a51d2942f7d05262d88b87734c2c50e1
murrayclifford <86817415+murrayclifford@users.noreply.github.com>
2023-10-30 14:37:06 +1100

    Updated logic and uninstall

--- Proactive Remediations/Visual Studio Code/Resolve-VSCodeUserInstall.ps1
+++ Proactive Remediations/Visual Studio Code/Resolve-VSCodeUserInstall.ps1
@@ -22,13 +22,13 @@ if (!([System.Environment]::Is64BitProcess)) {
 
 # Start Logging
 Start-Transcript -Path "$Env:Programdata\Microsoft\IntuneManagementExtension\Logs\Resolve-VSCodeUserInstall.log" -Append
-Write-Output "Info: Starting detection of VS Code user profile installations"
+Write-Output "Info: Starting removal of VS Code user profile installations"
 
 
 try{
 
     # Check whether VS Code is running on device
-    $VsCodeOpen = Get-Process -Name "Code" | Where-Object {$_.Path.StartsWith($Env:USERPROFILE)}
+    $VsCodeOpen = Get-Process -Name "Code" -ErrorAction SilentlyContinue | Where-Object {$_.Path.StartsWith($Env:USERPROFILE)}
     if($VsCodeOpen.Count -gt 0){
         Write-Output "Warn: VS Code process currently running. Exiting script"
         Stop-Transcript
@@ -36,7 +36,7 @@ try{
         Exit 0
     }
 
-    if($VsCodeOpen -eq 0){
+    if($null -eq $VsCodeOpen -or $VsCodeOpen -eq 0){
         # Gather array of user accounts on local device
         [System.Collections.ArrayList]$UserArray = (Get-ChildItem C:\Users\).Name
         $UserArray.Remove('Public')
@@ -46,8 +46,8 @@ try{
             $Path = "$Env:SystemDrive\Users\$User\AppData\Local\Programs\Microsoft VS Code\unins000.exe"
             if(Test-Path $Path){
                 Write-Output "Info: VS Code is installed for user $User"
-                $Args = '/SILENT /NORESTART /FORCECLOSEAPPLICATIONS /log="C:\ProgramData\Microsoft\IntuneManagementExtention\Logs\VSCodeUserUninstall.log'
-                Start-Process $Path -Args $Args -Wait
+                $Params = '/SILENT /NORESTART /FORCECLOSEAPPLICATIONS'
+                Start-Process $Path -Args $Params -Wait
                 Write-Output "Info: VS Code has been uninstalled for $User"
             }
             else{
