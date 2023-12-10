d93505c59ca331866f5acab8ceba018a9c9db8a3
murrayclifford <86817415+murrayclifford@users.noreply.github.com>
2023-12-11 10:19:36 +1100

    Update to include per-user registry check

--- Proactive Remediations/Visual Studio Code/Detect-VSCodeUserInstall.ps1
+++ Proactive Remediations/Visual Studio Code/Detect-VSCodeUserInstall.ps1
@@ -1,6 +1,7 @@
 <#
     .SYNOPSIS
-    Searches device for VS Code installations in user profiles 
+    Searches for Visual Studio Code (User) installation binaries and registry keys
+
 #>
 
 # Check if PowerShell is running as a 32-bit process and restart as a 64-bit process
@@ -20,33 +21,99 @@ if (!([System.Environment]::Is64BitProcess)) {
     }
 }
 
-# Start Logging
-Start-Transcript -Path "$Env:Programdata\Microsoft\IntuneManagementExtension\Logs\Detect-VSCodeUserInstall.log" -Append
-Write-Output "Starting detection of VS Code user profile installations"
+# Editable variables
+$App = "Visual Studio Code (User)"
+$LogPath = "$($Env:ProgramData)\Microsoft\IntuneManagementExtension\Logs"
+$LogName = "Resolve-VSCodeUserInstall.log"
+$LogIntro = "INFO: Starting detection of $($App)"
 
-# Loop through user profiles to check for VS Code installation binaries
+# Start Logging
+Start-Transcript -Path $LogPath\$LogName -Append
+Write-Output $LogIntro
+
+# Loop through user profiles to check for VS Code installation binaries and registry keys
 try {
-    # Run Test and store as variable
-    $Test = Get-ChildItem -Path "C:\Users\" -Filter "code.exe" -Recurse -Force -ErrorAction SilentlyContinue
+    #region VSCodeBinaries
+
+    # Run test and store as variable
+    $BinaryTest = Get-ChildItem -Path "C:\Users\" -Filter "code.exe" -Recurse -Force -ErrorAction SilentlyContinue
+    #endregion VSCodeBinaries
+
+    #region VSCodeRegistry
+
+    # Define regex pattern for user SIDs
+    $PatternSID = 'S-1-5-21-\d+-\d+\-\d+\-\d+$'
+
+    # Get Username, SID, and location of ntuser.dat for all users
+    Write-Output "INFO: Creating list of all user profiles on $($Env:ComputerName)"
+    $ProfileList = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*' | Where-Object { $_.PSChildName -match $PatternSID } | 
+        Select-Object  @{name = "SID"; expression = { $_.PSChildName } }, 
+        @{name = "UserHive"; expression = { "$($_.ProfileImagePath)\ntuser.dat" } }, 
+        @{name = "Username"; expression = { $_.ProfileImagePath -replace '^(.*[\\\/])', '' } }
+
+    # Get all user SIDs found in HKEY_USERS (ntuder.dat files that are loaded)
+    Write-Output "INFO: Creating list of user profiles that have been loaded into the registry"
+    $LoadedHives = Get-ChildItem Registry::HKEY_USERS | Where-Object { $_.PSChildname -match $PatternSID } | Select-Object @{name = "SID"; expression = { $_.PSChildName } }
+
+    # Get all users that are not currently logged
+    Write-Output "INFO: Creating list of user profiles that have not been loaded into the registry"
+    $UnloadedHives = Compare-Object $ProfileList.SID $LoadedHives.SID | Select-Object @{name = "SID"; expression = { $_.InputObject } }, UserHive, Username
+
+    # Loop through each profile on the machine
+    Write-Output "INFO: Checking user profiles on $($Env:ComputerName)"
+    Foreach ($Profile in $ProfileList) {
+        # Load User ntuser.dat if it's not already loaded
+        if ($Profile.SID -in $UnloadedHives.SID) {
+            Write-Output "INFO: Loading $($Profile.Username) profile"
+            reg load HKU\$($Profile.SID) $($Profile.UserHive) | Out-Null
+        }
+    
+        # Define regitry key location
+        $VSCodeKey = "Registry::HKU\$($Profile.SID)\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{771FD6B0-FA20-440A-A002-3B3BAC16DC50}_is1"
+
+        # Check for registry key and remove if found
+        Write-Output "INFO: Checking for $($App) registry key"
+        if(Test-Path $VSCodeKey){
+            Write-Output "INFO: Found $($App) registry key in $($Profile.SID)"
+            $RegistryTest = "Non-compliant"
+        }
+        
+        # Unload ntuser.dat        
+        if ($Profile.SID -in $UnloadedHives.SID) {
+            # Garbage collection and closing of ntuser.dat
+            [gc]::Collect()
+            reg unload HKU\$($Profile.SID) | Out-Null
+        }
+        
+        # Break profile loop when VS Code registry keys found for any user
+        if($RegistryTest){
+            Write-Output "INFO: $App registry keys found, exiting profile loop"
+            break
+        }
+    }
+    #endregion VSCodeRegistry
  
     # Check where test is compliant or not - if no instances of VS Code are discovered then mark as 'Compliant' and exit with 0
-    if ($null -eq $Test) {
-        Write-Output "Compliant"
+    if (($BinaryTest) -OR ($RegistryTest)) {
+        Write-Output "Non-compliant: $App binaries or registry keys found on $($Env:ComputerName)"
         Stop-Transcript
-        exit 0
+        Write-Output "Non-compliant: $App binaries or registry keys found on $($Env:ComputerName)"
+        Exit 1
     }
     # If instances of VS Code are discovered then mark as 'Non Compliant' and exit with 1
     else {
-        Write-Warning "Non Compliant"
+        Write-Warning "Compliant: $App binaries or registry keys not found on $($Env:ComputerName)"
         Stop-Transcript
-        exit 1
+        Write-Warning "Compliant: $App binaries or registry keys not found on $($Env:ComputerName)"
+        Exit 0
     }
 }
  
 catch {
     # Write errors messages to the log and exit
-    $errMsg = $_.exeption.essage
+    $errMsg = "ERR: Script execution failed"
     Write-Output $errMsg
     Stop-Transcript
+    Write-Output $errMsg
     Exit 2000
 }
